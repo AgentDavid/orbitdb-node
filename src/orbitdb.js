5a8e1dc5699d85498a8d0c1ea188d5eca9d2d29b
David Gimenez <alejandrogmnz@gmail.com>
2022-01-24 14:51:41 -0400

    identities

--- src/orbitdb.js
+++ src/orbitdb.js
@@ -1,23 +1,39 @@
 const OrbitDB = require("orbit-db");
-const IpfsClient = require("ipfs-http-client");
+const IPFS = require("ipfs");
+const Identities = require("orbit-db-identity-provider");
+
+const ipfsOptions = {
+  EXPERIMENTAL: {
+    pubsub: true,
+  },
+};
+
+const options = { id: "test1" };
 
 async function main() {
   // Create IPFS instance
-  const ipfs = IpfsClient.create({
-    host: "localhost",
-    port: 5001,
-    protocol: "http",
-  });
+  const ipfs = await IPFS.create(ipfsOptions);
 
+  //Test IPFS instance
   const results = await ipfs.add("IPFS works!!!");
   const cid = results.cid.toV1().toString();
   console.log("CID created via ipfs.add:", cid);
 
+  // Create identity
+  const identity = await Identities.createIdentity(options);
+
   // Create OrbitDB instance
-  const orbitdb = await OrbitDB.createInstance(ipfs);
+  const orbitdb = await OrbitDB.createInstance(ipfs, { identity: identity });
+
+  const optionsToWrite = {
+    accessController: {
+      type: "orbitdb", //OrbitDBAccessController
+      write: [orbitdb.identity.id],
+    },
+  };
 
   // Create database instance
-  const db = await orbitdb.keyvalue("Awesome");
+  const db = await orbitdb.keyvalue("Awesome", optionsToWrite);
   await db.load();
 
   let i = 0;
@@ -27,10 +43,10 @@ async function main() {
     await db.put(`key-${i}`, { data: "example" });
     await db.load();
     i++;
-  }, 4000);
+  }, 10000);
 
   db.events.on("replicated", (address) => {
-    console.log(db.iterator({ limit: -1 }).collect());
+    console.log("replicated");
   });
 
   console.log("database address: " + db.address.toString());
